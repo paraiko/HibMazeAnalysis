cf51246df7887a7859c0b2427fbd41011fda9fa7
A.S. Boerema <paraiko@gmail.com>
2016-01-29 22:13:20 +0100

    cleanup script and skip intermediate text output

--- hibmaze.py
+++ hibmaze.py
@@ -9,82 +9,51 @@ import re
 
 #Make a function for the decimals to binary conversion
 def decbin(DecNum):
-	#inv=np.invert(np.array[DecNum])
 	Bin=binary_repr(DecNum, width=8)
 	return Bin
 #Finished defining the decbin function
 
 #Define file name and give a name to the output file
-InFileName = '201010100.txt'
-OutFileName = InFileName + 'bin.txt'
+InFileName = '201010100_test.txt' #sys.argv[1]
+#OutFileName = InFileName + 'bin.txt' 
 
 InFile = open(InFileName, 'r')
-OutFile = open(OutFileName, 'w')
+#OutFile = open(OutFileName, 'w')
+
+day = list()
 
 #loop through each line in the file
 for Line in InFile:
 	#Strip the line endings and white space
 	Line=Line.strip()
-	
+	linelist= list()	
 	#Split by one more occurrence of a space
 	ElementList = re.split(" +", Line)
-	#print ElementList
 	
-	#Manier om data duidelijker te maken:
-	Time=str(ElementList[0])+str(ElementList[1]).zfill(2)+ \
+	#Format the date in one string add trailing zeros and separate milliseconds by:
+	DateTime=str(ElementList[0])+str(ElementList[1]).zfill(2)+ \
 	str(ElementList[2]).zfill(2)+str(ElementList[3]).zfill(2)+ \
-	str(ElementList[4]).zfill(2)+ str(ElementList[5]).zfill(2)+ \
-	str(ElementList[6]).zfill(3)
+	str(ElementList[4]).zfill(2)+ str(ElementList[5]).zfill(2)
 	
-	OutFile.write(Time + '\t')
+	millisec = str(ElementList[6]).zfill(3)
+	#OutFile.write(Time + '\t')
+		
+	#append datatime on the first position of the linelist
+	linelist.append(DateTime)
+	linelist.append(millisec)
 	
-	#for each column in the range of 7 until 13,
+	binstring = ""
+	#convert the channels to binary data and store in array on 3rd position for each column in the range of 7 until 13,
 	for Column in range(7,13):
 		#write the binary number in integer form, followed by an end-of-line in the OutFile
-		OutFile.write(decbin(int(ElementList[Column])))
-		#OutFile.write(int(decbin(int(ElementList[Column])))) ?
+		binstring += decbin(int(ElementList[Column]))
 		
-		#print the same output into the terminal
-		#print decbin(int(ElementList[Column])),
-	OutFile.write('\n')
-	
-print "Output printed to outfile"
-	
-#Close the files
-InFile.close()
-OutFile.close()
-
-#Poging tot het maken van een list van de output van de vorige loop
-OutFile = open(OutFileName, 'r')
-
-OutFileName2= OutFileName + 'bin.txt'
-OutFile2 = open(OutFileName2, 'w')
-
-#This for loop is made to create a list which will contain all data for one day
-#Which will contain an array for each
-
-#Make a new list, named 'dag'
-dag =list()
-#for each line in OutFile
-for line in OutFile:
-	#Make a new list, named 'linelist'
-	linelist= list()
-	#Of each Line in OutFile, strip off the end-of-line signs
-	line = line.strip('\n')
-	# Split each line based on tabs
-	# which gives two elements: the date/time column and the channel-output column
-	# and put them into 'col'
-	col = re.split("\t", line)
-	#append the integer of the first element of 'col' (date/time) to the list 'linelist'
-	linelist.append(int(col[0]))
-	
-	#make a new list named 'channel'
 	channel = list()
-	
 	# for each character in col[1] (i) in range 0-47,
 	# append the integer of it to the list 'channel'
 	for i in range(0,47):
-		ch = int(col[1][i])
+		#ch = int(col[1][i])
+		ch = int(binstring[i])
 		if ch > 0:
 			ch = 0
 		else:
@@ -95,14 +64,19 @@ for line in OutFile:
 	# append the list 'channel' (3rd dimension) to the list 'linelist' (2nd dimension)
 	linelist.append(channel)
 	# append the list 'linelist' to the list 'dag' (1st dimension)
-	dag.append(linelist)
+	day.append(linelist)
+	
+print "Output printed to outfile"
+	
+#Close the files
+InFile.close()
 
 #print the 3D list 'dag'	
-print dag	
+print day	
 
 #loop through each line of the file to write it to OutFile2:
-for line in dag:
-	OutFile2.write("%s\n" % line)
+#for line in dag:
+#	OutFile2.write("%s\n" % line)
 
 
 #OutFile2.write(DatumTijd + '\t' + ListBinary + '\n')
