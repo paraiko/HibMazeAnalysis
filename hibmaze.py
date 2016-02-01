1b198eb5f14460ae20e2b990be7291854f832b0d
A.S. Boerema <paraiko@gmail.com>
2016-02-01 21:12:48 +0100

    first attempt to do binsize aggregation

--- hibmaze.py
+++ hibmaze.py
@@ -2,7 +2,8 @@
 
 from numpy import binary_repr
 import numpy as np
-#import os #maybe for the more files
+import time
+from operator import add
 
 #maybe import re to load regular expression module
 import re
@@ -14,7 +15,7 @@ def decbin(DecNum):
 #Finished defining the decbin function
 
 #Define file name and give a name to the output file
-InFileName = '201010100_test.txt' #sys.argv[1]
+InFileName = '201010100_test.txt' # change a later stage to input from commandline or directory loop (sys.argv[1])
 #OutFileName = InFileName + 'bin.txt' 
 
 InFile = open(InFileName, 'r')
@@ -66,17 +67,130 @@ for Line in InFile:
 	# append the list 'linelist' to the list 'dag' (1st dimension)
 	day.append(linelist)
 	
-print "Output printed to outfile"
-	
-#Close the files
+#Close the input file
 InFile.close()
 
-#print the 3D list 'dag'	
-print day	
 
+# aggregate over specified binsize (in sec) and output in new file
+binsize = 600 #(10 min)
+nrbins = int(86400 / binsize)
+binnr = 0
+
+#get the date from the filename and convert
+datestr = InFileName[0:8]
+unitnr = InFileName[8:9]
+currday = time.strptime(datestr,"%Y%m%d")
+startsec = int(time.mktime(currday))
+binstart = startsec
+binend = startsec + binsize
+
+
+
+#loop over 1 day in binsize increments
+
+#count the amount of eventsobjects in the day list
+agr_day = list() # create new array for aggregated data 
+
+#create list with 48 empty channels for use later
+ech = list()
+for i in range(0,47):
+	ech.append(0)
+
+firstBinEvent = True
+
+#iterate over the events in de day list
+for (i, event) in enumerate(day):
+	
+	#create a list for the aggreated data of the bin ([0]= seconds sinds start, [1] array with channeldata
+	#bindata = list() 
+	#convert datetime to seconds sinds epoch and add the milliseconds
+	eventt = time.strptime(day[i][0],"%Y%m%d%H%M%S") 
+	eventsec = int(time.mktime(eventt)) #+ float(day[i][1]) /1000
+	#print binend - eventsec
+	
+	# add empty bins if necessary
+	print (eventsec-startsec)/nrbins
+	while (binstart <= eventsec-601):
+		bindata = list()
+		bindata.append(binnr)
+		bindata.append(ech)
+		agr_day.append(bindata)
+		
+		#increase with binsize
+		binstart += binsize
+		binend += binsize
+		firstBinEvent = True
+		binnr += 1
+		
+		#print str(binnr) + "empty event"
+		#print bindata
+	
+	if eventsec < binend :
+		#print str(eventsec) + " " + str(binend)
+		bindata = list()
+		if firstBinEvent :
+			#add new line with empth channels
+			bindata = list()
+			bindata.append(binnr)
+			bindata.append(ech)
+			agr_day.append(bindata)
+			
+			#add the existing channel list with the values in the current events.
+			agr_day[binnr][1] = map(add, agr_day[binnr][1], day[i][2])
+			firstBinEvent = False
+			
+			print str(binnr) + "first event"
+			#print bindata
+			#print day[i][2]
+		
+		else :
+			agr_day[binnr][1] = map(add, agr_day[binnr][1], day[i][2])
+			#print str(binnr) + "second event"
+		
+	else :
+		#proceed to next bin
+		#increase with binsize
+		binstart += binsize
+		binend += binsize
+		firstBinEvent = True 
+		binnr += 1
+		
+		#add new line with empty channels
+		bindata = list()
+		bindata.append(binnr)
+		bindata.append(ech)
+		agr_day.append(bindata)
+		agr_day[binnr][1] = map(add, agr_day[binnr][1], day[i][2])
+		firstBinEvent = False
+		print str(binnr) + "first event, next"
+		
+# print agr_day
+		
+
+		
+	
+
+
+
+
+
+# sum the 17 channels of 1 maze (frst maze starts at channel 0 2nd maze at channgel 24
+
+	
+	
+
+
+
+print "Output printed to outfile"
+#print day
+
+OutFileName = InFileName + '_agr.txt' 
+
+
+OutFile = open(OutFileName, 'w')
 #loop through each line of the file to write it to OutFile2:
-#for line in dag:
-#	OutFile2.write("%s\n" % line)
+for bin in agr_day:
+	OutFile.write("%s\n" % bin)
 
 
 #OutFile2.write(DatumTijd + '\t' + ListBinary + '\n')
